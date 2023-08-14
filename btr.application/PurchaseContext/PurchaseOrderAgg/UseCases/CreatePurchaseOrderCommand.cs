d4f83cde819e9092943ea7a59c921182b4e3cd6f
Jude <drury_yl@yahoo.com>
2023-08-14 18:33:35 +0700

    Start Develop Purchase Order (Backend)

---
+++ btr.application/PurchaseContext/PurchaseOrderAgg/UseCases/CreatePurchaseOrderCommand.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace btr.application.PurchaseContext.PurchaseOrderAgg.UseCases
+{
+    public class CreatePurchaseOrderCommand : IRequest<CreatePurchaseOrderResponse>
+    {
+        public string SupplierId { get; set; }
+        public string WarehouseId { get; set; }
+        public List<CreatePurchaseOrderCommandItem> ListItem { get; set; }
+    }
+
+    public class CreatePurchaseOrderResponse
+    {
+        public string PurchaseOrderId { get; set; }
+    }
+
+    public class CreatePurchaseOrderHandler : IRequestHandler<CreatePurchaseOrderCommand, CreatePurchaseOrderResponse>
+    {
+        public Task<CreatePurchaseOrderResponse> Handle(CreatePurchaseOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- btr.domain/PurchaseContext/PurchaseOrderAgg/PurchaseOrderModel.cs
+++ btr.domain/PurchaseContext/PurchaseOrderAgg/PurchaseOrderModel.cs
@@ -1,11 +1,32 @@
-﻿namespace btr.domain.PurchaseContext.PurchaseOrderAgg
+﻿using btr.domain.BrgContext.BrgAgg;
+using btr.domain.PurchaseContext.SupplierAgg;
+
+namespace btr.domain.PurchaseContext.PurchaseOrderAgg
 {
-    public class PurchaseOrderModel
+    public class PurchaseOrderModel : IPurchaseOrderKey, ISupplierKey
     {
         public string PurchaseOrderId { get; set; }
-        public string PurchaseDate { get; set; }
+        public string PurchaseOrderDate { get; set; }
         public string UserId { get; set; }
-        public string PrinsipalId { get; set; }
-        public string PrinsipalName { get; set; }
+        public string SupplierId { get; set; }
+        public string SupplierNameName { get; set; }
+    }
+
+    public class PurchaseOrderItemModel : IPurchaseOrderKey, IBrgKey
+    {
+        public string PurchaseOrderId { get; set; }
+        public string PurchaseOrderItemId { get; set; }
+        public int NoUrut { get; set; }
+        public string BrgId { get; set; }
+        public string BrgName { get; set; }
+        public int Qty { get; set; }
+        public string Satuan { get; set; }
+        public decimal Harga { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal DiskonProsen { get; set; }
+        public decimal DiskonRp { get; set; }
+        public decimal TaxProsen { get; set; }
+        public decimal TaxRp { get; set; }
+        public decimal Total { get; set; }
     }
 }
