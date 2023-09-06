62bb6366d9dfbb5cec37428a9f1cfc10afd9edd3
Jude <drury_yl@yahoo.com>
2023-09-06 11:14:13 +0700

    Develop Import Opname

---
+++ btr.application/SupportContext/PlaygroundAgg/IImportOpnameDal.cs
@@ -0,0 +1,15 @@
+﻿using btr.domain.SupportContext.PlaygroundAgg;
+using btr.nuna.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btr.application.SupportContext.PlaygroundAgg
+{
+    public interface IImportOpnameDal :
+        IListData<ImportOpnameModel>
+    {
+    }
+}
--- btr.distrib/SharedForm/TestPlayground.cs
+++ btr.distrib/SharedForm/TestPlayground.cs
@@ -1,29 +1,98 @@
-﻿using btr.application.InventoryContext.StokAgg;
+﻿using btr.application.BrgContext.BrgAgg;
+using btr.application.InventoryContext.OpnameAgg;
+using btr.application.InventoryContext.StokAgg;
+using btr.application.SupportContext.PlaygroundAgg;
+using btr.domain.BrgContext.BrgAgg;
+using btr.domain.InventoryContext.OpnameAgg;
+using btr.domain.InventoryContext.WarehouseAgg;
+using btr.domain.SupportContext.UserAgg;
 using MediatR;
+using System.Linq;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace btr.distrib.SharedForm
 {
     public partial class TestPlayground : Form
     {
-        public readonly IMediator _mediator;
-        public TestPlayground(IMediator mediator)
+        private readonly IMediator _mediator;
+        private readonly IOpnameBuilder _opnameBuilder;
+        private readonly IOpnameWriter _opnameWriter;
+        private readonly IImportOpnameDal _importOpnameDal;
+        private readonly IBrgDal _brgDal;
+
+        public TestPlayground(IMediator mediator,
+            IOpnameBuilder opnameBuilder,
+            IOpnameWriter opnameWriter,
+            IImportOpnameDal importOpnameDal,
+            IBrgDal brgDal)
         {
             InitializeComponent();
             _mediator = mediator;
+            _opnameBuilder = opnameBuilder;
+            _opnameWriter = opnameWriter;
+            _importOpnameDal = importOpnameDal;
+            _brgDal = brgDal;
+            button1.Click += Button1_Click;
         }
 
-        private async void button1_Click(object sender, System.EventArgs e)
+        private void Button1_Click(object sender, System.EventArgs e)
         {
-            //var cmd = new AddStokCommand("BR0001", "W19", 10, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
-            //await _mediator.Send(cmd);
-
-            //var cmd2 = new AddStokCommand("BR0001", "W19", 4, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
-            //await _mediator.Send(cmd2);
+            throw new System.NotImplementedException();
+        }
 
 
-            //var cmd3 = new RemoveStokCommand("BR0001", "W19", 13, "PCS/40", 2100, "FAKTUR-001", "FAKTUR");
-            //await _mediator.Send(cmd3);
+
+        //private async void button1_Click(object sender, System.EventArgs e)
+        //{
+        //    //var listImport = _importOpnameDal.ListData()?.ToList();
+        //    //PrgBar.Maximum = listImport.Count;
+        //    //PrgBar.Value = 0;
+        //    //foreach(var item in listImport)
+        //    //{
+        //    //    PrgBar.Value++;
+        //    //    var brg = _brgDal.GetData(item.BrgCode);
+
+        //    //    var opname = _opnameBuilder
+        //    //        .Create()
+        //    //        .Brg(brg)
+        //    //        .Warehouse(new WarehouseModel("G00"))
+        //    //        .User(new UserModel("jude7"))
+        //    //        .QtyAwal(0, 0)
+        //    //        .QtyOpname(0, item.Qty)
+        //    //        .Nilai(item.Nilai)
+        //    //        .Build();
+        //    //    _opnameWriter.Save(ref opname);
+        //    //    GenStok(opname);
+        //    //}
+
+        //    //var cmd = new AddStokCommand("BR0001", "W19", 10, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
+        //    //await _mediator.Send(cmd);
+
+        //    //var cmd2 = new AddStokCommand("BR0001", "W19", 4, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
+        //    //await _mediator.Send(cmd2);
+
+
+        //    //var cmd3 = new RemoveStokCommand("BR0001", "W19", 13, "PCS/40", 2100, "FAKTUR-001", "FAKTUR");
+        //    //await _mediator.Send(cmd3);
+        //}
+        private async void GenStok(OpnameModel opname)
+        {
+            var qtyAdjust = opname.Qty2Adjust * opname.Conversion2;
+            qtyAdjust += opname.Qty1Adjust;
+
+            if (qtyAdjust > 0)
+            {
+                var cmd = new AddStokCommand(opname.BrgId, opname.WarehouseId,
+                    qtyAdjust, opname.Satuan1, opname.Nilai, opname.OpnameId, "OPNAME");
+                await _mediator.Send(cmd);
+            }
+            else
+            {
+                var cmd = new RemoveStokCommand(opname.BrgId, opname.WarehouseId,
+                    -qtyAdjust, opname.Satuan1, 0, opname.OpnameId, "OPNAME");
+                await _mediator.Send(cmd);
+            }
         }
     }
 }
