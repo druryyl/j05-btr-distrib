4032d206519db7844995b23ade93654e96e33d85
Jude <drury_yl@yahoo.com>
2023-09-07 00:51:30 +0700

    Develop Tools Generate Opname

--- btr.distrib/SharedForm/TestPlayground.cs
+++ btr.distrib/SharedForm/TestPlayground.cs
@@ -6,6 +6,7 @@ using btr.domain.BrgContext.BrgAgg;
 using btr.domain.InventoryContext.OpnameAgg;
 using btr.domain.InventoryContext.WarehouseAgg;
 using btr.domain.SupportContext.UserAgg;
+using btr.nuna.Application;
 using MediatR;
 using System.Linq;
 using System.Windows.Forms;
@@ -33,49 +34,51 @@ namespace btr.distrib.SharedForm
             _opnameWriter = opnameWriter;
             _importOpnameDal = importOpnameDal;
             _brgDal = brgDal;
-            button1.Click += Button1_Click;
+            button1.Click += button1_Click;
         }
 
-        private void Button1_Click(object sender, System.EventArgs e)
+
+        private async void button1_Click(object sender, System.EventArgs e)
         {
-            throw new System.NotImplementedException();
+            var listImport = _importOpnameDal.ListData()?.ToList();
+            PrgBar.Maximum = listImport.Count;
+            PrgBar.Value = 0;
+            using (var trans = TransHelper.NewScope())
+            {
+                foreach (var item in listImport)
+                {
+                    PrgBar.Value++;
+                    if (item.Qty <= 0)
+                        continue;
+
+                    var brg = _brgDal.GetData(item.BrgCode);
+
+                    var opname = _opnameBuilder
+                        .Create()
+                        .Brg(brg)
+                        .Warehouse(new WarehouseModel("G00"))
+                        .User(new UserModel("jude7"))
+                        .QtyAwal(0, 0)
+                        .QtyOpname(0, item.Qty)
+                        .Nilai(item.Nilai)
+                        .Build();
+                    _opnameWriter.Save(ref opname);
+                    GenStok(opname);
+                }
+
+                trans.Complete();
+            }
+
+            //var cmd = new AddStokCommand("BR0001", "W19", 10, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
+            //await _mediator.Send(cmd);
+
+            //var cmd2 = new AddStokCommand("BR0001", "W19", 4, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
+            //await _mediator.Send(cmd2);
+
+
+            //var cmd3 = new RemoveStokCommand("BR0001", "W19", 13, "PCS/40", 2100, "FAKTUR-001", "FAKTUR");
+            //await _mediator.Send(cmd3);
         }
-
-
-
-        //private async void button1_Click(object sender, System.EventArgs e)
-        //{
-        //    //var listImport = _importOpnameDal.ListData()?.ToList();
-        //    //PrgBar.Maximum = listImport.Count;
-        //    //PrgBar.Value = 0;
-        //    //foreach(var item in listImport)
-        //    //{
-        //    //    PrgBar.Value++;
-        //    //    var brg = _brgDal.GetData(item.BrgCode);
-
-        //    //    var opname = _opnameBuilder
-        //    //        .Create()
-        //    //        .Brg(brg)
-        //    //        .Warehouse(new WarehouseModel("G00"))
-        //    //        .User(new UserModel("jude7"))
-        //    //        .QtyAwal(0, 0)
-        //    //        .QtyOpname(0, item.Qty)
-        //    //        .Nilai(item.Nilai)
-        //    //        .Build();
-        //    //    _opnameWriter.Save(ref opname);
-        //    //    GenStok(opname);
-        //    //}
-
-        //    //var cmd = new AddStokCommand("BR0001", "W19", 10, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
-        //    //await _mediator.Send(cmd);
-
-        //    //var cmd2 = new AddStokCommand("BR0001", "W19", 4, "PCS/40", 1900, "ADJ-001", "ADJUSTMENT");
-        //    //await _mediator.Send(cmd2);
-
-
-        //    //var cmd3 = new RemoveStokCommand("BR0001", "W19", 13, "PCS/40", 2100, "FAKTUR-001", "FAKTUR");
-        //    //await _mediator.Send(cmd3);
-        //}
         private async void GenStok(OpnameModel opname)
         {
             var qtyAdjust = opname.Qty2Adjust * opname.Conversion2;
