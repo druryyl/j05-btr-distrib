693f930fb253e3fba9e62d27fc5dd8ca8ffb98aa
Jude <drury_yl@yahoo.com>
2023-09-23 20:56:26 +0700

    Develop Void Nomor Faktur Pajak (tidak bisa digunakan lagi)

---
+++ btr.distrib/SharedForm/VoidReasonForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace btr.distrib.SharedForm
+{
+    public partial class VoidReasonForm : Form
+    {
+        public VoidReasonForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
