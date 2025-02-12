389c0eeea886dd4bbfb276a1c5b1c4fa17d95aa8
Jude <drury_yl@yahoo.com>
2025-02-12 15:31:11 +0700

    feat: print-preview faktur dibuat default 100%. Dan saat selesai print tidak nge-close form preview (antisipasi cetak ulang)

--- btr.distrib/SalesContext/FakturAgg/RdlcViewerForm.cs
+++ btr.distrib/SalesContext/FakturAgg/RdlcViewerForm.cs
@@ -14,28 +14,21 @@ namespace btr.distrib.SalesContext.FakturAgg
         {
             InitializeComponent();
             TheViewer.Print += TheViewer_Print;
-
-            //  set margin page
             PageSettings pageSettings = new PageSettings
             {
-                Margins = new Margins(25, 25, 25, 25) // Left, Right, Top, Bottom
+                // Left, Right, Top, Bottom
+                Margins = new Margins(25, 25, 25, 25)
             };
             TheViewer.SetPageSettings(pageSettings);
         }
 
         private void TheViewer_Print(object sender, ReportPrintEventArgs e)
         {
-            this.Close();
+            //this.Close();
         }
 
         public void SetReportData(string reportName, List<ReportDataSource> listDatasource)
         {
-            //var reportBasePath = _paramSistemDal
-            //    .GetData(new ParamSistemModel("REPORT_BASE_PATH"))
-            //    ?.ParamValue ?? string.Empty;
-            //var clientId = _paramSistemDal.GetData(new ParamSistemModel("CLIENT_ID"))
-            //    ?.ParamValue ?? string.Empty;
-
             var reportFileName = $"{reportName}.rdlc";
             var reportFileFullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports", reportFileName);
 
@@ -43,10 +36,10 @@ namespace btr.distrib.SalesContext.FakturAgg
             TheViewer.LocalReport.ReportPath = reportFileFullPath;
 
             TheViewer.ProcessingMode = ProcessingMode.Local;
-            TheViewer.SetDisplayMode(DisplayMode.PrintLayout); // Print layout is generally non-interactive
-            TheViewer.ZoomMode = ZoomMode.PageWidth; // Simplify 
+            TheViewer.SetDisplayMode(DisplayMode.PrintLayout);
+            TheViewer.ZoomMode = ZoomMode.PageWidth;
             TheViewer.ZoomMode = ZoomMode.Percent;
-            TheViewer.ZoomPercent = 150;
+            TheViewer.ZoomPercent = 100;
 
             TheViewer.RefreshReport();
         }
