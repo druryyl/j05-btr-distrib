9cc1ddd729dee66964c6b69761e2464bcf82a3d6
Jude <drury_yl@yahoo.com>
2025-02-18 06:57:53 +0700

    feat: info-stok-balance menampilkan stok nol

--- btr.infrastructure/InventoryContext/StokBalanceRpt/StokBalanceViewDal.cs
+++ btr.infrastructure/InventoryContext/StokBalanceRpt/StokBalanceViewDal.cs
@@ -18,31 +18,29 @@ namespace btr.infrastructure.InventoryContext.StokBalanceRpt
         public IEnumerable<StokBalanceView> ListData()
         {
             const string sql = @"
-                SELECT      aa.BrgId, aa.WarehouseId, aa.Qty,
-                            ISNULL(bb.BrgName, '') AS BrgName,
-                            ISNULL(bb.BrgCode, '') AS BrgCode,
-                            ISNULL(bb.SupplierId, '') AS SupplierId,
-                            ISNULL(bb.KategoriId, '') AS KategoriId,
-                            ISNULL(bb.Hpp, 0) AS Hpp,
+                SELECT      aa.BrgId, aa.BrgName, aa.BrgCode, 
+                            aa.SupplierId, aa.KategoriId, aa.Hpp,
+                            ISNULL(bb.WarehouseId, '') WarehouseId, 
+                            ISNULL(bb.Qty, 0) Qty,
                             ISNULL(cc1.Satuan, '') AS SatKecil,
                             ISNULL(cc2.Satuan, '') AS SatBesar,
                             ISNULL(cc2.Conversion, 1) AS Conversion,
                             ISNULL(dd.WarehouseName, '') AS WarehouseName,
                             ISNULL(ee.KategoriName, '') AS KategoriName,
                             ISNULL(ff.SupplierName, '') AS SupplierName
-                FROM        BTR_StokBalanceWarehouse aa
-                LEFT JOIN   BTR_Brg bb ON aa.BrgId = bb.BrgId
+                FROM        BTR_Brg aa 
+                LEFT JOIN   BTR_StokBalanceWarehouse bb ON bb.BrgId = aa.BrgId
                 LEFT JOIN   (
                             SELECT  DISTINCT BrgId, Satuan, 1 AS Conversion
                             FROM    BTR_BrgSatuan
-                            WHERE   Conversion = 1) cc1 ON aa.BrgId = cc1.BrgId
+                            WHERE   Conversion = 1) cc1 ON bb.BrgId = cc1.BrgId
                 LEFT JOIN   (
                             SELECT  DISTINCT BrgId, Satuan, Conversion
                             FROM    BTR_BrgSatuan
-                            WHERE   Conversion > 1) cc2 ON aa.BrgId = cc2.BrgId
-                LEFT JOIN   BTR_Warehouse dd ON aa.WarehouseId = dd.WarehouseId
-                LEFT JOIN   BTR_Kategori ee ON bb.KategoriId = ee.KategoriId
-                LEFT JOIN   BTR_Supplier ff ON bb.SupplierId = ff.SupplierId ";
+                            WHERE   Conversion > 1) cc2 ON bb.BrgId = cc2.BrgId
+                LEFT JOIN   BTR_Warehouse dd ON bb.WarehouseId = dd.WarehouseId
+                LEFT JOIN   BTR_Kategori ee ON aa.KategoriId = ee.KategoriId
+                LEFT JOIN   BTR_Supplier ff ON aa.SupplierId = ff.SupplierId ";
             
             using(var conn = new SqlConnection(ConnStringHelper.Get(_opt)))
             {
